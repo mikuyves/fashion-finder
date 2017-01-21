d9f3734e971d613b76de3c7a74eda12b8f58a96b
mikuyves <mikuyves@gmail.com>
2017-01-22 00:43:40 +0800

    Add logging in main_spider

--- eshop/eshop/spiders/main_spider.py
+++ eshop/eshop/spiders/main_spider.py
@@ -1,15 +1,18 @@
 # -*- coding: utf-8 -*-
 import os
+import logging
+import logging.config
 
 from twisted.internet import reactor
 from scrapy.crawler import CrawlerRunner
 from scrapy.utils.project import get_project_settings
 
 
-
 if __name__ == '__main__':
     runner = CrawlerRunner(get_project_settings())
     from eshop.settings import PROJECT_PATH
+    logging.config.fileConfig(os.path.join(PROJECT_PATH, 'log.conf'))
+    logger = logging.getLogger('eshop')
 
     with open(os.path.join(PROJECT_PATH, 'urls.txt')) as f:
         urls = f.read().split('\n')
